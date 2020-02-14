74321c2dfee05959c1823b0d751cc8e27988260c
kaitlyn-c <kaicom@vt.edu>
2020-02-14 16:26:33 -0500

    changes made. my trying shit out file is becoming a mess.

--- kaitlyn_trying_shit_out.py
+++ kaitlyn_trying_shit_out.py
@@ -1,15 +1,22 @@
 # %% Imports and file load
 import pathlib, math, numpy as np, pandas as pd, seaborn as sns, matplotlib, matplotlib.pyplot as plt
 
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import train_test_split
-from xgboost import XGBClassifier
-from sklearn import metrics
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor, GradientBoostingClassifier
+from sklearn.linear_model import LogisticRegression, RidgeClassifier
+from sklearn.svm import SVC
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import GridSearchCV, cross_val_score, train_test_split
+from sklearn.feature_selection import SelectKBest, chi2, mutual_info_classif, mutual_info_regression, RFECV
+from sklearn.feature_extraction import FeatureHasher
+from sklearn import metrics, preprocessing
+from scipy import stats
+
+le_make = preprocessing.LabelEncoder()
+le_model = preprocessing.LabelEncoder()
+le_cat = preprocessing.LabelEncoder()
+oh_make = preprocessing.OneHotEncoder()
+oh_model = preprocessing.OneHotEncoder()
 
 sns.set()
 %matplotlib inline
@@ -23,19 +30,16 @@ midrange = ['ACURA','BUICK','CHEVROLET','DODGE','FORD','GMC','HUMMER','JEEP','LI
 lowend = ['CHRYSLER','FIAT','HONDA','HYUNDAI','KIA','MAZDA','MERCURY','MITSUBISHI','NISSAN','PONTIAC','SCION','SUZUKI']
 budget = ['DAEWOO','EAGLE','GEO','ISUZU','OLDSMOBILE','PLYMOUTH','SAAB','SATURN','SMART']
 
-popularities = data.purchase_make.value_counts().to_frame().reset_index()
-popularities.purchase_make = popularities.purchase_make / popularities.purchase_make.sum()
-popularities.rename(columns={'purchase_make':'make_popularity'},inplace=True)
+make_popularities = data.purchase_make.value_counts().to_frame().reset_index()
+model_popularities = data.purchase_model.value_counts().to_frame().reset_index()
+make_popularities.rename(columns={'purchase_make':'make_popularity'},inplace=True)
 
 
-data = data.merge(popularities, how='left', left_on='purchase_make', right_on='index')
+data = data.merge(make_popularities, how='left', left_on='purchase_make', right_on='index')
 data.drop(columns=['index'],inplace=True)
 
-data['purchase_make_cat'] = np.nan
-data.loc[data['purchase_make'].isin(luxury),'purchase_make_cat'] = 4
-data.loc[data['purchase_make'].isin(midrange),'purchase_make_cat'] = 3
-data.loc[data['purchase_make'].isin(lowend),'purchase_make_cat'] = 2
-data.loc[data['purchase_make'].isin(budget),'purchase_make_cat'] = 1
+data.loc[data.customer_distance_to_dealer == '?', 'customer_distance_to_dealer'] = np.nan
+data['customer_distance_to_dealer'] = data['customer_distance_to_dealer'].astype('float64')
 
 data['customer_age'] = data['customer_age'].map({'0 - 20':1,
                                                 '21 - 30':2,
@@ -82,22 +86,21 @@ data['purchase_price'] = data['purchase_price'].map({'0 - 5000':1,
                                                     '?':np.nan})
 data['customer_gender'] = data['customer_gender'].map({'M':0,'F':1,'U':np.nan})
 
-
-
-#data.groupby('purchase_make')['insert_num'].count()
-'''purchase_make = data.purchase_make
-purchase_model = data.purchase_model
-
-data[['purchase_make', 'purchase_model']] = data[['purchase_make', 'purchase_model']].astype('category')
-data['purchase_make'] = data['purchase_make'].cat.codes
-data['purchase_model'] = data['purchase_model'].cat.codes
-'''
-
-data.loc[data.customer_distance_to_dealer == '?', 'customer_distance_to_dealer'] = np.nan
-data['customer_distance_to_dealer'] = data['customer_distance_to_dealer'].astype('float64')
-
 data['loyal'] = 0
 data.loc[data.subsequent_purchases > 0, 'loyal'] = 1
+
+data['purchase_make_cat'] = np.nan
+data.loc[data['purchase_make'].isin(luxury),'purchase_make_cat'] = 'Luxury'
+data.loc[data['purchase_make'].isin(midrange),'purchase_make_cat'] = 'Mid-Range'
+data.loc[data['purchase_make'].isin(lowend),'purchase_make_cat'] = 'Low-End'
+data.loc[data['purchase_make'].isin(budget),'purchase_make_cat'] = 'Budget'
+
+
+# %% Run if numerics needed
+
+data['purchase_make'] = le_make.fit_transform(data.purchase_make)
+data['purchase_model'] = le_model.fit_transform(data.purchase_model)
+data['purchase_make_cat'] = le_cat.fit_transform(data.purchase_make_cat)
 
 # %% Determining how to handle missing values
 
@@ -136,6 +139,8 @@ def randomboolean(rand):
 data.groupby(pd.isnull(data.customer_gender))['loyal'].count()
 #data.groupby(pd.isnull(data.customer_gender))['loyal'].mean()
 #data.groupby('loyal')['customer_gender'].mean()
+data['gender_missing'] = 0
+data.loc[pd.isnull(data.customer_gender), 'gender_missing'] = 1
 data['customer_gender'].fillna(randomboolean(np.random.rand()), inplace=True)
 
 # Missing values for income are fairly large. They don't affect the mean of the
@@ -154,24 +159,88 @@ data['customer_income'].fillna(data['customer_income'].median(), inplace=True)
 #data.loc[data.post_purchase_satisfaction == '?','survey_missing'] = 1
 data.drop(columns=['post_purchase_satisfaction'], inplace=True)
 
-# %%
-features = data.drop(columns=['loyal','subsequent_purchases','purchase_make','purchase_model','insert_num'])
+
+# %% Separate Feature Sets
+cat_features = data[['purchase_make','purchase_model','customer_gender','purchase_make_cat','trade_in','vehicle_financing','distance_missing','income_missing','gender_missing','customer_previous_purchase','vehicle_warranty_used','distance_binned']]
+cat_label = data['loyal']
+
+num_features = data[['purchase_vehicle_year','purchase_price','customer_age','customer_income','customer_distance_to_dealer','make_popularity']]
+num_label = data['subsequent_purchases']
+
+################################################################################
+# UNIVARIATE FEATURE SELECTION
+################################################################################
+
+# %% chi2
+bestfeatures = SelectKBest(score_func=chi2, k='all')
+fit = bestfeatures.fit(cat_features,cat_label)
+dfscores = pd.DataFrame(fit.scores_)
+dfpvalues = pd.DataFrame(fit.pvalues_)
+dfcolumns = pd.DataFrame(cat_features.columns)
+#concat two dataframes for better visualization
+featureScores = pd.concat([dfcolumns,dfscores,dfpvalues],axis=1)
+featureScores.columns = ['Specs','Score','Pval']  #naming the dataframe columns\
+featureScores.sort_values('Score',ascending=False)
+
+sns.heatmap(data.corr().abs(),square=True)
+
+sns.scatterplot(data=data,x='subsequent_purchases',y='customer_income')
+
+for i in ['purchase_vehicle_year','purchase_price','customer_age','customer_income','customer_distance_to_dealer','make_popularity']:
+    print(i)
+    print(stats.kendalltau(data[i],data['subsequent_purchases']))
+
+for i in ['purchase_make','purchase_model','customer_gender','purchase_make_cat','trade_in','vehicle_financing','distance_missing','income_missing','gender_missing','customer_previous_purchase','vehicle_warranty_used','distance_binned']:
+    print(i)
+    groupednumbers = {}
+    for grp in data[i].unique():
+        groupednumbers[grp] = data['loyal'][data[i]==grp].values
+    args = groupednumbers.values()
+
+    print(stats.kruskal(*args))
+
+
+
+###############################################################################
+# MACHINE LEARNING MODEL SELECTION
+###############################################################################
+
+# %% RandomForestClassifier
+prefeatures = data.drop(columns=['loyal','subsequent_purchases','insert_num','distance_missing'])
+postfeatures = prefeatures.drop(columns=['gender_missing','income_missing','purchase_make_cat','customer_age'])
+postfeatures.columns
 labels = data['loyal']
 
 # %%
+features = postfeatures
 X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.4, random_state=42)
 X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size=0.5, random_state=42)
 
-rf = RandomForestClassifier(n_estimators=50, max_depth=10)
+rf = RandomForestClassifier(n_estimators=30, max_depth=5)
 rf.fit(X_train, y_train)
 scores = rf.score(X_val, y_val)
 predictions = rf.predict(X_test)
-metrics.accuracy_score(y_test, predictions)
 metrics.classification_report(y_test, predictions, labels=[1,0])
 rf.feature_importances_
 X_test.columns
+metrics.accuracy_score(y_test, predictions)
+
+data.purchase_model.unique()
 
 # %%
+features = postfeatures
+X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.4, random_state=42)
+X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size=0.5, random_state=42)
+
+model = RidgeClassifier()
+model.fit(X_train, y_train)
+scores = model.score(X_val, y_val)
+predictions = model.predict(X_test)
+metrics.classification_report(y_test, predictions, labels=[1,0])
+metrics.accuracy_score(y_test, predictions)
+
+# %%
+
 '''
 # %% Regressor test
 features = data.drop(columns=['loyal','purchase_make','purchase_model','subsequent_purchases','distance_missing','customer_distance_to_dealer','insert_num'])
@@ -190,26 +259,57 @@ X_test.columns
 metrics.confusion_matrix(y_test, predictions2, labels=[1,0])
 '''
 
-# %% Follow-up analysis
-sns.heatmap(data.drop(columns=['insert_num','subsequent_purchases','loyal','purchase_make','purchase_model']).corr('pearson').abs(),cmap='Blues',square=True)
+###############################################################################
 
-
-data.groupby('distance_binned')['loyal'].mean()
-data.groupby(['subsequent_purchases','distance_binned'])['insert_num'].count()
-
-# %% double check feature importance
-from sklearn.feature_selection import SelectKBest
-from sklearn.feature_selection import chi2
-from sklearn.feature_selection import f_classif
-
-bestfeatures = SelectKBest(score_func=chi2, k=10)
+# %%
+rf = RandomForestClassifier(n_estimators=30,max_depth=10)
+lr = LogisticRegression(solver='lbfgs')
+bestfeatures = RFECV(rf)
 fit = bestfeatures.fit(features,labels)
-dfscores = pd.DataFrame(fit.scores_)
+dfscores = pd.DataFrame(fit.ranking_)
 dfcolumns = pd.DataFrame(features.columns)
+supp = pd.DataFrame(fit.support_)
 #concat two dataframes for better visualization
-featureScores = pd.concat([dfcolumns,dfscores],axis=1)
-featureScores.columns = ['Specs','Score']  #naming the dataframe columns\
-featureScores.sort_values('Score',ascending=False).to_csv('output.csv')
+featureScores = pd.concat([dfcolumns,dfscores,supp],axis=1)
+featureScores.columns = ['Specs','Score','Support']  #naming the dataframe columns
+featureScores.sort_values('Score',ascending=True)
+# Plot number of features VS. cross-validation scores
+plt.figure()
+plt.xlabel("Number of features selected")
+plt.ylabel("Cross validation score (nb of correct classifications)")
+plt.plot(range(1, len(bestfeatures.grid_scores_) + 1), bestfeatures.grid_scores_)
+plt.show()
+
+# %%
+rf = RandomForestClassifier()
+lr = LogisticRegression(solver = 'sag')
+bestfeatures = RFECV(rf)
+fit = bestfeatures.fit(features,num_label)
+dfscores = pd.DataFrame(fit.ranking_)
+dfcolumns = pd.DataFrame(features.columns)
+supp = pd.DataFrame(fit.support_)
+#concat two dataframes for better visualization
+featureScores = pd.concat([dfcolumns,dfscores,supp],axis=1)
+featureScores.columns = ['Specs','Score','Support']  #naming the dataframe columns
+featureScores.sort_values('Score',ascending=True)
+# Plot number of features VS. cross-validation scores
+plt.figure()
+plt.xlabel("Number of features selected")
+plt.ylabel("Cross validation score (nb of correct classifications)")
+plt.plot(range(1, len(bestfeatures.grid_scores_) + 1), bestfeatures.grid_scores_)
+plt.show()
+
+################################################################################
+################################################################################
+
+
+
+
+
+
+
+
+
 
 # %%
 sns.set()
@@ -227,3 +327,6 @@ sns.countplot(x='customer_previous_purchase',data=data,hue='loyal')
 sns.heatmap(data.pivot_table(index='purchase_price',columns=['customer_age'],values='loyal',aggfunc='mean'),cmap='Blues',square=True)
 data.pivot_table(index='customer_income',columns=['purchase_price'],values='loyal',aggfunc='mean').to_csv('output.csv')
 popularities.to_csv('output.csv')
+
+data.pivot_table(index='make_popularity',values='customer_distance_to_dealer', aggfunc='mean').to_csv('output.csv')
+data.groupby('subsequent_purchases').mean()
